f19fd0a83bdc21523f252cde347648731c5af9f2
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-10-11 18:20:26 +0200

    feat db: basic functional interface structure created

--- db/functions.sql
+++ db/functions.sql
@@ -5,7 +5,7 @@ BEGIN
     RETURN (SELECT TIMESTAMP '2023-10-05 00:00:00' + random() * (TIMESTAMP '2023-10-11 00:00:00' - TIMESTAMP '2023-10-05 00:00:00'));
 END $$ LANGUAGE plpgsql;
 
-/* FUNCTIONS */
+/* QUESTION */
 -- new_question
 DROP FUNCTION IF EXISTS new_question(QUESTION.TITLE%TYPE, CONTENT.DATA%TYPE, MEDUSER.USERNAME%TYPE, TAG.NAME%TYPE);
 CREATE FUNCTION new_question(
@@ -34,6 +34,27 @@ BEGIN
     RETURN question_id;
 END $$ LANGUAGE plpgsql;
 
+-- get_question_from_author
+DROP FUNCTION IF EXISTS get_question_from_author(MEDUSER.ID%TYPE);
+CREATE FUNCTION get_question_from_author(
+    author_id MEDUSER.ID%TYPE
+) RETURNS SETOF QUESTION
+AS $$
+BEGIN
+    RETURN QUERY SELECT * FROM QUESTION WHERE AUTHOR = author_id;
+END $$ LANGUAGE plpgsql;
+
+/* USER */
+-- get_user_id
+DROP FUNCTION IF EXISTS get_user_id(MEDUSER.USERNAME%TYPE);
+CREATE FUNCTION get_user_id(
+    user_name MEDUSER.USERNAME%TYPE
+) RETURNS MEDUSER.ID%TYPE
+AS $$
+BEGIN
+    RETURN (SELECT ID FROM MEDUSER WHERE USERNAME = user_name);
+END $$ LANGUAGE plpgsql;
+
 -- new_user
 DROP FUNCTION IF EXISTS new_user(MEDUSER.USERNAME%TYPE);
 CREATE FUNCTION new_user(
@@ -47,6 +68,44 @@ BEGIN
     -- INSERT INTO MEDUSER (USERNAME) VALUES (username) RETURNING ID INTO user_id;
     INSERT INTO MEDUSER (USERNAME, CREATED) VALUES (username, random_date()) RETURNING ID INTO user_id;
     RETURN user_id;
+END $$ LANGUAGE plpgsql;
+
+/* VOTE */
+-- vote_content
+DROP FUNCTION IF EXISTS vote_content(CONTENT.ID%TYPE, MEDUSER.ID%TYPE, BOOLEAN, BOOLEAN);
+CREATE FUNCTION vote_content(
+    content CONTENT.ID%TYPE,
+    user_name MEDUSER.ID%TYPE,
+    vote BOOLEAN,
+    up_vote BOOLEAN
+) RETURNS VOID
+AS $$
+BEGIN
+    IF vote THEN
+        IF EXISTS (SELECT * FROM VOTE WHERE CONTENT_ID = content AND USER_ID = user_name) THEN
+            UPDATE VOTE SET UPVOTE = up_vote WHERE CONTENT_ID = content AND USER_ID = user_name;
+        ELSE
+            INSERT INTO VOTE (CONTENT_ID, USER_ID, UPVOTE) VALUES (content, user_name, up_vote);
+        END IF;
+    ELSE
+        DELETE FROM VOTE WHERE CONTENT_ID = content AND USER_ID = user_name;
+    END IF;
+END $$ LANGUAGE plpgsql;
+
+-- vote_question
+DROP FUNCTION IF EXISTS vote_question(QUESTION.ID%TYPE, MEDUSER.ID%TYPE, BOOLEAN, BOOLEAN);
+CREATE FUNCTION vote_question(
+    question_id QUESTION.ID%TYPE,
+    user_id MEDUSER.ID%TYPE,
+    vote BOOLEAN,
+    upvote BOOLEAN
+) RETURNS VOID
+AS $$
+DECLARE
+    content_id CONTENT.ID%TYPE;
+BEGIN
+    content_id := (SELECT QUESTION_CONTENT_ID FROM QUESTION WHERE ID = question_id);
+    PERFORM vote_content(content_id, user_id, vote, upvote);
 END $$ LANGUAGE plpgsql;
 
 -- todo update_question
