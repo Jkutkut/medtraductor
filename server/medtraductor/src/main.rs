a78d5bbeacd30fb975a99c3fdf098b71e838c061
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-10-15 20:31:49 +0200

    feat server: dbquery

--- server/medtraductor/src/main.rs
+++ server/medtraductor/src/main.rs
@@ -3,6 +3,72 @@ use rocket::{Build, Rocket, launch, routes, catchers, get, tokio};
 use rocket::serde::json::Json;
 
 mod config;
+// mod api;
+mod api {
+	use rocket::{get, routes, serde::json::Json, http::CookieJar, State};
+	use crate::config::route_error::InvalidAPI;
+	use rocket::serde::{Deserialize, Serialize};
+	use tokio_postgres::{Client};
+	use std::collections::HashMap;
+	use uuid::Uuid;
+
+	pub fn get_v1_routes() -> Vec<rocket::Route> {
+		routes![
+			dbquery
+		]
+	}
+
+	#[derive(Debug, Deserialize)]
+	#[serde(crate = "rocket::serde")]
+	struct DBQuery {
+		query: String,
+	}
+
+	type DBQueryRequest = Json<DBQuery>;
+	#[derive(Debug, Serialize)]
+	enum ResponseTypes {
+		Str(String),
+		Arr(Vec<ResponseTypes>),
+		Int(i32),
+		Obj(HashMap<String, ResponseTypes>),
+	}
+	type DBQueryResponse = Json<HashMap<String, ResponseTypes>>;
+
+	#[get("/dbquery", format = "json", data = "<request>")]
+	async fn dbquery(
+		db: &State<Client>,
+		cookies: &CookieJar<'_>,
+		request: DBQueryRequest,
+	) -> Result<DBQueryResponse, InvalidAPI> {
+		println!("request: {:?}", request);
+		let mut size = 0;
+		let mut result_arr = Vec::new();
+		let stmt = db.prepare(&request.query).await.unwrap();
+		for row in db.query(&stmt, &[]).await.unwrap() {
+			let row_size = row.len();
+			let mut vec = Vec::new();
+			for i in 0..row_size {
+				match row.try_get::<_, String>(i) {
+					Ok(value) => vec.push(ResponseTypes::Str(value)),
+					Err(e) => {
+						eprintln!("Error while getting value from DB");
+						eprintln!("{:?}", e);
+					}
+				}
+			}
+			result_arr.push(ResponseTypes::Arr(vec));
+			size += 1;
+		}
+		let mut result: DBQueryResponse = Json(HashMap::new());
+		result.0.insert("size".to_string(), ResponseTypes::Int(size));
+		result.0.insert(
+			"data".to_string(),
+			ResponseTypes::Arr(result_arr)
+		);
+		Ok(result)
+	}
+
+}
 
 pub const ENV: &'static str = "/installation/.env";
 
@@ -10,7 +76,6 @@ pub const ENV: &'static str = "/installation/.env";
 fn ping() -> Json<&'static str> {
 	Json("pong")
 }
-
 
 #[launch]
 async fn rocket() -> Rocket<Build> {
@@ -53,7 +118,7 @@ async fn rocket() -> Rocket<Build> {
 		.attach(config::cors::CORS)
 		.manage(db_client)
 		.mount("/", routes![ping, config::cors::options])
-		// .mount("/api/v1", api::get_v1_routes())
+		.mount("/api/v1", api::get_v1_routes())
 		.register("/api", catchers![
 			config::route_error::api_not_implemented,
 			config::route_error::internal_server_error
